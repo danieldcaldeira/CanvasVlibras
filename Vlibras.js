6087621b3d8f32c1dc7cd3f2ecc158890fbbb7f7
Daniel Caldeira <danieldcaldeira@gmail.com>
2022-05-19 17:23:21 -0300

    Create Vlibras.js

---
+++ Vlibras.js
@@ -0,0 +1,20 @@
+let vlibrasScript = document.createElement('script');
+vlibrasScript.setAttribute("src", "https://vlibras.gov.br/app/vlibras-plugin.js");    
+document.body.appendChild(vlibrasScript);
+vlibrasScript.onload = () => {
+    CreateDOMObjects();
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+
+}
+
+let CreateDOMObjects = () => {
+    const DOM = `
+        <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+        </div>
+    `;
+    document.body.insertAdjacentHTML('beforeend', DOM);
+}
